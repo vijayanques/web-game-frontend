9bc2ca2c240a2b9006b59d894d205c114345ec3f
Aditya <aditya.anques@gmail.com>
2026-05-11 12:14:18 +0530

    Force dynamic rendering for SEO metadata - disable all caching

---
+++ src/app/category/[slug]/layout.tsx
@@ -0,0 +1,15 @@
+import { generateMetadata } from './metadata'
+
+// Force dynamic rendering - no caching
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
+export { generateMetadata }
+
+export default function CategoryLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return children
+}
